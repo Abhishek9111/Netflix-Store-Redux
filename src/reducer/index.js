e30f723891f69371fa7db9379a61a22816a160cc
Abhishek Kapoor <kapoorabhishek567@gmail.com>
2022-07-16 22:53:30 +0530

    used combineReducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux'
 import list from './list'
 
-export default combineReducers({
+export default combineReducers({        //used to create a single function combining those three functions to be passed to createStore
     list
 })
