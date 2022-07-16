49674d1d33250a153d7e9675f5ab35fc17cf34ed
Abhishek Kapoor <kapoorabhishek567@gmail.com>
2022-07-16 22:31:50 +0530

    created store

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = ()=>{
+  return(
+    <ScrollView>
+      <Text>Hi</Text>
+    </ScrollView>
+  )
+}
+
+export default App;
